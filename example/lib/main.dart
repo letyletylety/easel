bd777141c5f11f979255be39af5b059783adc867
Lety <taej0127@gmail.com>
2022-06-14 12:57:22 +0900

    example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:easel_example/src/easel.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main(List<String> args) {
   runApp(const TestApp());
