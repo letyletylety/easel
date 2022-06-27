ea44cbaaffbac40371a975c1025bbc9991fe732c
lety <taej0127@gmail.com>
2022-06-27 23:22:01 +0900

    segment precision change -> 10

--- lib/src/drawable/drawables/segment.dart
+++ lib/src/drawable/drawables/segment.dart
@@ -1,11 +1,14 @@
 import 'package:easel/easel.dart';
 
 /// the segment between two pixels;
+///
+/// if both the positions of two pixels same,
+/// then just draw the pixel a only;
 class Segment extends Drawable with HasPrecision {
   final Pixel a;
   final Pixel b;
 
-  Segment(this.a, this.b, [int precision = 1]) {
+  Segment(this.a, this.b, {int precision = 10}) {
     changePrecision(precision);
   }
 }
