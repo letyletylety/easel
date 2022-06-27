43bcffb825e8ac10ec2cb4081872db6a3c399881
lety <taej0127@gmail.com>
2022-06-27 19:56:10 +0900

    add segment

--- lib/src/drawable/drawables/segment.dart
+++ lib/src/drawable/drawables/segment.dart
@@ -1,9 +1,11 @@
 import 'package:easel/easel.dart';
 
 /// the segment between two pixels;
-class Segment extends Drawable {
+class Segment extends Drawable with HasPrecision {
   final Pixel a;
   final Pixel b;
 
-  Segment(this.a, this.b);
+  Segment(this.a, this.b, [int precision = 1]) {
+    changePrecision(precision);
+  }
 }
